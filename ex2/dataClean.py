aba25588c377274082625b3d29cb2a55d9242c28
Noa levitzky <45270253+noalevitzky@users.noreply.github.com>
2020-05-11 18:16:46 +0300

    Add files via upload
    did most of 1

--- ex2/dataClean.py
+++ ex2/dataClean.py
@@ -1,8 +1,37 @@
 import pandas as pd
 
-filePath = "PriceFull7290696200003-097-202005040319-001.csv"
-table = pd.read_csv(filePath)
-print(table.isna())
-print("razi")
+victory_price_full_1 = "PriceFull7290696200003-097-202005040319-001.csv"
+t_victory_prices_full_1 = pd.read_csv(victory_price_full_1)
 
+victory_price_full_2 = "PriceFull7290696200003-077-202005040327-001.csv"
+t_victory_prices_full_2 = pd.read_csv(victory_price_full_2)
 
+victory_promo_full_1 = "PromoFull7290696200003-097-202005040319-001.csv"
+t_victory_promo_full_1 = pd.read_csv(victory_promo_full_1)
+
+"""missing files"""
+# print(t_victory_prices_full_1.isna())
+# print(t_victory_prices_full_1.isnull())
+# print(t_victory_promo_full_1.isna())
+
+"""corrupted files"""
+# todo: add example
+
+"""different units"""
+# print(pd.DataFrame(t_victory_promo_full_1, columns=["DiscountRate"]))
+# DiscountRate = pd.DataFrame(t_victory_promo_full_1)["DiscountRate"]
+# print("column name: DiscountRate\nmin value: " + str(min(DiscountRate)) +
+# "\nmax value: " + str(max(DiscountRate)))
+
+"""unrelated data"""
+# Items = pd.DataFrame(t_victory_prices_full_1)["ItemName"]
+# print(Items)
+
+"""mixed string names and code names"""
+# ManufactureName1 = pd.DataFrame(t_victory_prices_full_1, columns=["ItemCode", "ManufactureName"])
+# ManufactureName2 = pd.DataFrame(t_victory_prices_full_2, columns=["ItemCode", "ManufactureName"])
+# print(ManufactureName1.iloc[8], "\n\n", ManufactureName2.iloc[7])
+
+"""unavailable data"""
+# Manufacture = pd.DataFrame(t_victory_prices_full_2, columns=["ItemCode", "ManufactureName"])
+# print(Manufacture.iloc[671])
