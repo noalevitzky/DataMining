c0d21e5f29046539e3953f6b4cb90fae64e4cbf6
Noa levitzky <45270253+noalevitzky@users.noreply.github.com>
2020-06-18 17:05:36 +0300

    Add files via upload

--- ex3/code.py
+++ ex3/code.py
@@ -6,14 +6,19 @@ import nltk
 from nltk.stem import PorterStemmer
 from stop_words import get_stop_words
 from nltk.tokenize import PunktSentenceTokenizer
+import re
 
 # General
 plt.style.use('fivethirtyeight')
-FILE_PATH = 'theWoderfulWizardOfOz.txt'
+FILE_PATH = 'C:/Users/Noa/Desktop/huji/second year/dataMining/ex3/theWoderfulWizardOfOz.txt'
+NOUNS_PATH = 'C:/Users/Noa/Desktop/huji/second year/dataMining/ex3/nouns.txt'
 porter = PorterStemmer()
+STOP_SIGNS = [',', '.', "'", '"', '`', ';', "''", '""', "``", ']', '[', '(',
+              ')', ':', "?", "!"]
 
 with open(FILE_PATH, 'r') as file:
     tokens = nltk.word_tokenize(file.read())
+
 
 # Helper Functions
 def stem_sentence(t):
@@ -22,11 +27,13 @@ def stem_sentence(t):
         stem_sentence.append(porter.stem(word))
     return stem_sentence
 
-# TODO: find a solution for punctuations and random chars.
-# At the very least we should remove [.,'].
+
 def get_ordered_freq(t):
     frequency = {}
     for word in t:
+        if word in STOP_SIGNS:
+            continue
+
         count = frequency.get(word, 0)
         frequency[word] = count + 1
 
@@ -34,6 +41,7 @@ def get_ordered_freq(t):
         frequency.items(), key=lambda item: item[1]))}
 
     return ordered
+
 
 def select_x_highest(d, num):
     i = 0
@@ -46,9 +54,11 @@ def select_x_highest(d, num):
         else:
             return highest_dict
 
-def print_freq(f, section):
-    print("**** ", section, " ****\nmost common tokens:\n", f,
-          "\nnum of tokens: ", str(len(f.keys())),"\n")
+
+def print_freq(f, num, section):
+    print("**** ", section, " ****\ntop 20 tokens:\n", f,
+          "\nnum of tokens: ", num, "\n")
+
 
 # For POS tagging
 def process_content(tokenized_text, chunk):
@@ -59,12 +69,14 @@ def process_content(tokenized_text, chunk):
             tagged = nltk.pos_tag(words)
             chunkParser = nltk.RegexpParser(chunk)
             chunked = chunkParser.parse(tagged)
-            for subtree in chunked.subtrees(filter=lambda t: t.label() == 'Chunk'):
+            for subtree in chunked.subtrees(
+                    filter=lambda t: t.label() == 'Chunk'):
                 processed.append(subtree)
     except Exception as e:
         print(str(e))
     finally:
         return processed
+
 
 def extract_phrases(nltk_chunked_txt):
     phrases = []
@@ -76,55 +88,49 @@ def extract_phrases(nltk_chunked_txt):
         phrases.append(" ".join(word_lst))
     return phrases
 
+def extract_words(nltk_chunked_txt):
+    phrases = []
+    for row in nltk_chunked_txt:
+        row_lst = row.leaves()
+        for word, t in row_lst:
+            phrases.append(word)
+    return phrases
+
+def parseSection(f_dict, section, title):
+    freq_y = np.array(list(f_dict.values()))
+    rank_x = np.arange(1, len(f_dict) + 1)
+    fig, axes = plt.subplots()
+    axes.loglog(rank_x, freq_y, marker=".", linestyle='None')
+
+    plt.title(title)
+    plt.ylabel('Word Frequency (log)')
+    plt.xlabel('Rank (log)')
+    # plt.show()
+    res = select_x_highest(f_dict, 20)
+    print_freq(res, len(f_dict), section)
+
+
 if __name__ == "__main__":
     with open(FILE_PATH, 'r') as file:
-
         """ B - tokenize"""
         ordered_freq = get_ordered_freq(tokens)
-        freq_y = np.array(list(ordered_freq.values()))
-        rank_x = np.arange(1, len(ordered_freq) + 1)
-        fig, axes = plt.subplots()
-        axes.loglog(rank_x, freq_y, marker=".", linestyle='None')
-
-        plt.title('Frequency of Words In The Wizard of Oz')
-        plt.ylabel('Word Frequency (log)')
-        plt.xlabel('Rank (log)')
-        plt.show()
-        res = select_x_highest(ordered_freq, 20)
-        print("Top 20 tokens are:", res)
+        parseSection(ordered_freq, 'B',
+                     'Frequency of Words In The Wizard of Oz')
 
         """ C - remove stopwords"""
         tokens_no_sw = \
             [w for w in tokens if w not in get_stop_words('english')]
         ordered_freq_no_sw = get_ordered_freq(tokens_no_sw)
-        freq_y = np.array(list(ordered_freq_no_sw.values()))
-        rank_x = np.arange(1, len(ordered_freq_no_sw) + 1)
-
-        fig, axes = plt.subplots()
-        plt.loglog(rank_x, freq_y, marker=".", linestyle='None')
-
-        plt.title('Frequency of Words In The Wizard of Oz (Without Stopwords)')
-        plt.ylabel('Word Frequency (log)')
-        plt.xlabel('Rank (log)')
-        plt.show()
-        res = select_x_highest(ordered_freq_no_sw, 20)
-        print("Top 20 tokens are:", res)
+        parseSection(ordered_freq_no_sw, 'C',
+                     'Frequency of Words In The Wizard of Oz '
+                     '(Without Stopwords)')
 
         """ D - stem """
         tokens_no_sw_stemmed = stem_sentence(tokens_no_sw)
         ordered_freq_no_sw_stemmed = get_ordered_freq(tokens_no_sw_stemmed)
-        freq_y = np.array(list(ordered_freq_no_sw_stemmed.values()))
-        rank_x = np.arange(1, len(ordered_freq_no_sw_stemmed) + 1)
-
-        fig, axes = plt.subplots()
-        plt.loglog(rank_x, freq_y, marker=".", linestyle='None')
-
-        plt.title('Frequency of Words In The Wizard of Oz (w/ Stemming & w/o stopwords)')
-        plt.ylabel('Word Frequency (log)')
-        plt.xlabel('Rank (log)')
-        plt.show()
-        res = select_x_highest(ordered_freq_no_sw, 20)
-        print("Top 20 tokens are:", res)
+        parseSection(ordered_freq_no_sw_stemmed, 'D',
+                     'Frequency of Words In The Wizard of Oz '
+                     '(w/ Stemming & w/o stopwords)')
 
         """ E - POS """
         custom_sent_tokenizer = PunktSentenceTokenizer("GWBush.txt")
@@ -138,15 +144,32 @@ if __name__ == "__main__":
         phrases = extract_phrases(processed)
         phrases_freq = get_ordered_freq(phrases)
 
-        freq_y = np.array(list(phrases_freq.values()))
-        rank_x = np.arange(1, len(phrases_freq) + 1)
+        parseSection(phrases_freq, 'E',
+                     'Frequency of Phrases In The Wizard of Oz (Adj+Noun)')
 
-        fig, axes = plt.subplots()
-        plt.loglog(rank_x, freq_y, marker=".", linestyle='None')
+        """ F - example of a mistake in POS """
+        # todo
 
-        plt.title('Frequency of Phrases In The Wizard of Oz (Adj+Noun)')
-        plt.ylabel('Phrase Frequency (log)')
-        plt.xlabel('Rank (log)')
-        plt.show()
-        res = select_x_highest(phrases_freq, 20)
-        print("Top 20 tokens are:", res)
+        """ G - Tag cloud """
+        chunkPattern = r"""Chunk: {(<NN.>)} """
+        processed = process_content(tokenized, chunkPattern)
+        phrases = extract_words(processed)
+        phrases_freq = get_ordered_freq(phrases)
+        print(phrases_freq)
+        with open(NOUNS_PATH, 'w') as f:
+            f.write(" ".join(phrases))
+
+        """ H - re for two consecutive repeated words """
+        reg = re.compile(r'\b(\w+)[\'\<\>\|\.\?\-\"\,\:\;\[\]\(\)\{\}\\\!\~'
+                         r'\@\#\%\^\&\*\s]+\1\b')
+        repeated = []
+        with open(FILE_PATH, 'r') as file:
+            for line in file:
+                if reg.search(line):
+                    repeated.append(line.rstrip().lstrip())
+
+        print("**** H ****\nrepeated tokens (total " + str(len(repeated)) + "):")
+        for i, line in enumerate(repeated):
+            print(str(i+1) + ':', line)
+
+
