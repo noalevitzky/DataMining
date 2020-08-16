faca82c92b6cae5c2f84fc991285985ab195202b
Noa levitzky <45270253+noalevitzky@users.noreply.github.com>
2020-08-16 11:51:09 +0300

    Add files via upload

--- final project/code/preprocessor.py
+++ final project/code/preprocessor.py
@@ -13,7 +13,10 @@ CLEANED_POPULAR_PICKLE = 'C:/Users/Noa/Desktop/huji/second year/dataMining/final
 CLEANED_MIDDLE_PICKLE = 'C:/Users/Noa/Desktop/huji/second year/dataMining/final project/output/cleaned_middle_talks.p'
 CLEANED_UNPOPULAR_PICKLE = 'C:/Users/Noa/Desktop/huji/second year/dataMining/final project/output/cleaned_unpopular_talks.p'
 
-# cleaned csv
+# csv's
+UNCLEANED_POPULAR_CSV = 'C:/Users/Noa/Desktop/huji/second year/dataMining/final project/output/uncleaned_popular.csv'
+UNCLEANED_MIDDLE_CSV = 'C:/Users/Noa/Desktop/huji/second year/dataMining/final project/output/uncleaned_middle.csv'
+UNCLEANED_UNPOPULAR_CSV = 'C:/Users/Noa/Desktop/huji/second year/dataMining/final project/output/uncleaned_unpopular.csv'
 CLEANED_POPULAR_CSV = 'C:/Users/Noa/Desktop/huji/second year/dataMining/final project/output/cleaned_popular.csv'
 CLEANED_MIDDLE_CSV = 'C:/Users/Noa/Desktop/huji/second year/dataMining/final project/output/cleaned_middle.csv'
 CLEANED_UNPOPULAR_CSV = 'C:/Users/Noa/Desktop/huji/second year/dataMining/final project/output/cleaned_unpopular.csv'
@@ -77,38 +80,42 @@ def write_pickle(p_file, lst):
 
 if __name__ == '__main__':
     """ data cleaning """
-    # POPULAR
-    print('POPULAR')
     POPULAR = process_pickle(POPULAR_PICKLE)
-    print('original: ', len(POPULAR))
+    MIDDLE = process_pickle(MIDDLE_PICKLE)
+    UNPOPULAR = process_pickle(UNPOPULAR_PICKLE)
+
+    # write uncleaned csv
+    write_csv_without_transcript(UNCLEANED_POPULAR_CSV, POPULAR)
+    write_csv_without_transcript(UNCLEANED_MIDDLE_CSV, MIDDLE)
+    write_csv_without_transcript(UNCLEANED_UNPOPULAR_CSV, UNPOPULAR)
+
+    # POPULAR
+    print('POPULAR\noriginal: ', len(POPULAR))
     POPULAR = remove_talks_without_transcript(POPULAR)
     print('transcript:', len(POPULAR))
     POPULAR = remove_talks_with_missing_values(POPULAR)
     print('values:', len(POPULAR))
 
     # MIDDLE
-    print('\nMIDDLE')
-    MIDDLE = process_pickle(MIDDLE_PICKLE)
-    print('original: ', len(MIDDLE))
+    print('\nMIDDLE\noriginal: ', len(MIDDLE))
     MIDDLE = remove_talks_without_transcript(MIDDLE)
     print('transcript:', len(MIDDLE))
     MIDDLE = remove_talks_with_missing_values(MIDDLE)
     print('values:', len(MIDDLE))
 
     # UNPOPULAR
-    print('\nUNPOPUAR')
-    UNPOPULAR = process_pickle(UNPOPULAR_PICKLE)
-    print('original: ', len(UNPOPULAR))
+    print('\nUNPOPUAR\noriginal: ', len(UNPOPULAR))
     UNPOPULAR = remove_talks_without_transcript(UNPOPULAR)
     print('transcript:', len(UNPOPULAR))
     UNPOPULAR = remove_talks_with_missing_values(UNPOPULAR)
     print('values:', len(UNPOPULAR))
 
     # export to CSV without transcript
-    # export to pickle
     write_csv_without_transcript(CLEANED_POPULAR_CSV, POPULAR)
     write_csv_without_transcript(CLEANED_MIDDLE_CSV, MIDDLE)
     write_csv_without_transcript(CLEANED_UNPOPULAR_CSV, UNPOPULAR)
+
+    # export to pickle
     write_pickle(CLEANED_POPULAR_PICKLE, POPULAR)
     write_pickle(CLEANED_MIDDLE_PICKLE, MIDDLE)
     write_pickle(CLEANED_UNPOPULAR_PICKLE, UNPOPULAR)
